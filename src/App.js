181a1ffe778dfc7a18e9b8e2bafd73a6a166d0f5
Aaron <54789688+flowersa29@users.noreply.github.com>
2020-10-16 00:25:05 -0500

    first Push
    first push. Outline of login form

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React, { useState } from "react"
+import LoginForm from "./components/LoginForm.js"
+import "./App.css"
+
+function App() {
+  const adminUser = {
+    email: "admin@admin.com",
+    password: "admin123",
+  }
+  const [user, setUser] = useState({ name: "", email: "" })
+  const [error, setError] = useState("")
+  const Login = (details) => {
+    console.log(details)
+  }
+
+  const LogOut = () => {
+    console.log("logout")
+  }
+  return (
+    <div className="App">
+      {user.email != "" ? (
+        <div className="welcome">
+          <h2>
+            welcome,<span>{user.name}</span>
+          </h2>
+          <button>LogOut</button>
+        </div>
+      ) : (
+        <LoginForm />
+      )}
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/LoginForm.js
@@ -0,0 +1,29 @@
+import React from 'react'
+
+const LoginForm = () => {
+  return (
+    <form>
+      <div className="form-inner">
+        <h2>Login</h2>
+        {/*Error! */}
+        <div className="form-group">
+          <label htmlFor="name">Name:</label>
+          <input type="text" name="name" id="name"/>
+        </div>
+        <div className="form-group">
+            <label htmlFor="email">Email</label>
+            <input type="email" name="email" id="email"/>
+
+        </div>
+        <div className="form-group">
+          <label htmlFor="password">password</label>
+          <input type="password" name="password" id="password"/>
+
+        </div>
+      
+      </div>
+    </form>
+  )
+}
+
+export default LoginForm
