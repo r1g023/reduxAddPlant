19776c2b33f38d49ad5e0ae83f982d9c13b1120e
Aaron <54789688+flowersa29@users.noreply.github.com>
2020-10-19 19:33:11 -0500

    10/19
    PlantCard and plantlist components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import Header from "../src/components/Header/Header"
-
-
+import PlantCard from "./components/PlantCard"
+import PlantList from "./components/PlantList"
+import "./components/styles.css"
 import {
   BrowserRouter as Router,
   Switch,
@@ -9,16 +10,23 @@ import {
 } from "react-router-dom";
 import RegistrationForm from './components/Header/RegistrationForm';
 import LoginForm from './components/LoginForm';
+
+
+
+
+
 function App() {
+
 return (
     <Router>
     <div className="App">
       <Header/>
+      <PlantList />
         <div className="container d-flex align-items-center flex-column">
           <Switch>
             <Route path="/" exact={true}>
-              {/* <RegistrationForm /> */}
-              <LoginForm />
+              
+            
             </Route>
           </Switch>
        </div>
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,11 @@
 import React from 'react'
 
-const Header = () =>{
+const Header = (props) =>{
 
   return(
       <nav class="navbar nav-dark bg-primary">
         <div className="row col-12 d-flex justify-content-center text-white">
+          <h2>{props.text}</h2>
           <span className="h3">Register</span>
         </div>
       </nav>
